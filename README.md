b176e1d0cc1c133d192bd1a79386e40216491a59
Fatima Oliveira <Fatima.21.00@hotmail.com>
2024-04-19 12:49:45 +0100

    updated README and correlation added in script

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Project - Programming and Scripting
 
-Project analysis of the Iris dataset.
+This project is my analysis of the Iris dataset for the Module Programming and Scripting of the Course Data Analytics in [ATU Galway Mayo](https://www.gmit.ie/).
+
+This README follows the the documentation from [Github](https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-readmes) regarding README files.
+
+## What the project does
+
+This repository includes:
+1. a notebook with my explanation of the data set;
+2. a [Python script](analysis.py), that loads the data and outputs it in a presentable way through summary of variables, visual representation and correlation;
+3. a [txt file](Summary of each variable.txt) generated with Python, with the summary of the variables;
+3. images with the plotted data in histograms, bars and scatter plots, all generated with Python script.
+
+## Why the project is useful
+
+While doing this work, I learned many essential skills that I need to develop in order to become a data analyst. It helped me in how to use data sets, to explore some of the vast tools available in Python to manipulate and present the data, and to go deep in the observations of variables to describe them and maybe create a possible association between them.
+
+## How users can get started with the project
+
+To run the codes, [Anaconda](https://www.anaconda.com/) and [Visual Studio Code](https://code.visualstudio.com/) need to be installed to run these programs.
+
+## Where users can get help with your project
+
+If there's any issues with this repository, please contact me in the [issues](https://github.com/FatimaBOliveira/pands-project/issues) link.
+
+## Author
+
+Fatima Oliveira
+Email:g00438857@atu.ie or Fatima.21.00@hotmail.com
--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 # Analysis of Iris data set
-#
 # Author: Fatima Oliveira
+
+    # Libraries
 
 # Data frames.
 import pandas as pd
@@ -9,12 +10,14 @@ import matplotlib.pyplot as plt
 # Numerical arrays.
 import numpy as np
  
-# Loading the data
-    # For this I downloaded the Iris data set from URL in: https://archive.ics.uci.edu/static/public/53/iris.zip, and I extracted files from zip file and loaded to the machine.
+    # Loading the data
+
+# For this I downloaded the Iris data set from URL in: https://archive.ics.uci.edu/static/public/53/iris.zip.
+# I extracted the files from zip file into my pands-project folder/repository.
 path = "./iris/"
 logFilename = path + "iris.data"
 
-    # Column names taken considering the file "iris.names", in section 7 the attributes of each variable.
+# Column names given considering the file "iris.names", in section 7 the attributes of each variable.
 colNames= ("sepal_length_cm",
     "sepal_width_cm", 
     "petal_length_cm", 
@@ -22,24 +25,24 @@ colNames= ("sepal_length_cm",
     "class" 
 )
 
+# load the data set in this python script.
 df = pd.read_csv(logFilename, sep=',', header= None, names=colNames)
 
     # Summary of each variable
 
 a=df.describe()
-a.to_csv('Summary of each variable.txt', sep="\t", mode="w") # Will create txt file with the summary of the variables
+a.to_csv('Summary of each variable.txt', sep="\t", mode="w") # "w" will create txt file with the summary of the variables.
 # https://saturncloud.io/blog/how-to-write-a-pandas-dataframe-to-a-txt-file/
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_csv.html
 
-# Histogram
+    # Histogram and bar charts
 
 slen = df["sepal_length_cm"]
 swid = df["sepal_width_cm"]
 plen = df["petal_length_cm"]
 pwid = df["petal_width_cm"]
 
-    # Histogram of variables together.
-# https://matplotlib.org/stable/gallery/color/named_colors.html
+# Histogram of variables together.
 plt.hist(slen, label= "sepal length", edgecolor = "black", fill=False)
 plt.hist(swid, label= "sepal width", edgecolor = "red", fill=False)
 plt.hist(plen, label= "petal length", edgecolor = "cyan", fill=False)
@@ -48,14 +51,15 @@ plt.title("Iris data set")
 plt.xlabel("cm")
 plt.ylabel("counts")
 plt.legend()
-#plt.savefig("Histogram.png") # to save plot in the directory
+#plt.savefig("Histogram with variables together.png") # to save in the directory
+# https://matplotlib.org/stable/gallery/color/named_colors.html
 
-    # Separate Histogram of each variable.
+# Separate histograms of each variable.
 df1 = pd.DataFrame(slen)
 df2 = pd.DataFrame(swid)
 df3 = pd.DataFrame(plen)
 df4 = pd.DataFrame(pwid)
-fig, axes = plt.subplots(2,2, figsize=(20, 20))
+fig, axes = plt.subplots(2,2, figsize=(10, 6))
 # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.subplots.html
 # https://www.tutorialspoint.com/how-to-plot-two-histograms-side-by-side-using-matplotlib
 
@@ -63,39 +67,46 @@ df1.hist(ax=axes[0, 0], edgecolor = "black")
 df2.hist(ax=axes[0, 1], edgecolor = "red")
 df3.hist(ax=axes[1, 0], edgecolor = "cyan")
 df4.hist(ax=axes[1, 1], edgecolor = "lime")
-#plt.savefig("Histogram2.png") # to save plot in the directory
+#plt.savefig("Histograms of each variable.png") 
 plt.show()
 
-    # For the class of Iris I can use a bar.
-# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.bar.html
+# Bar chart - class of Iris.
 s = df["class"]
 values, counts = np.unique(s, return_counts=True)
 plt.bar(values, counts, color=["green", "red","blue"])
-plt.legend()
+plt.title("Class of Iris")
+#plt.savefig("Class of Iris bar chart.png")
 plt.show()
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.bar.html
 
+    # Scatter plots
 
-# Scatter plot of each pair of variables
+# Create a new figure with the scatter plots. 
+fig, axs = plt.subplots(2, 3, figsize=(10, 6))
 # https://matplotlib.org/stable/gallery/lines_bars_and_markers/scatter_star_poly.html#sphx-glr-gallery-lines-bars-and-markers-scatter-star-poly-py
-
-    # Create a new figure with the scatter plots. 
-fig, axs = plt.subplots(2, 3, sharex=True, sharey=True, figsize=(10, 10))
-
+# https://python-charts.com/correlation/scatter-plot-matplotlib/?utm_content=cmp-true
 # Plot them together
-
-axs[0, 0].scatter(slen, swid, edgecolors=["black","red"])
-axs[0, 0].set_title("sepal lenght and sepal width")
+axs[0, 0].scatter(slen, swid, edgecolors=["black","red"], label=["sepal lenght", "sepa width"])
+axs[0, 0].set_title("Sepal lenght and sepal width")
 axs[0, 1].scatter(slen, plen, edgecolors=["black","cyan"])
-axs[0, 1].set_title("sepal lenght and petal lenght")
+axs[0, 1].set_title("Sepal lenght and petal lenght")
 axs[0, 2].scatter(slen, pwid, edgecolors=["black","lime"])
-axs[0, 2].set_title("sepal lenght and petal width")
+axs[0, 2].set_title("Sepal lenght and petal width")
 axs[1, 0].scatter(swid, plen, edgecolors=["red","cyan"])
-axs[1, 0].set_title("sepal width and petal lenght")
+axs[1, 0].set_title("Sepal width and petal lenght")
 axs[1, 1].scatter(swid, pwid, edgecolors=["red","lime"])
-axs[1, 1].set_title("sepal width and petal width")
+axs[1, 1].set_title("Sepal width and petal width")
 axs[1, 2].scatter(plen, pwid, edgecolors=["cyan","lime"])
-axs[1, 2].set_title("petal lenght and petal width")
-
+axs[1, 2].set_title("Petal lenght and petal width")
 plt.show()
 
-# Analysis
+    # Correlation
+
+print(np.corrcoef(slen, swid))
+print(np.corrcoef(slen, plen))
+print(np.corrcoef(slen, pwid))
+print(np.corrcoef(swid, plen))
+print(np.corrcoef(swid, pwid))
+print(np.corrcoef(plen, pwid))
+
+#https://numpy.org/doc/stable/reference/generated/numpy.corrcoef.html
